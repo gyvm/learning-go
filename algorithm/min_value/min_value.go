c1e9d77bb119507eca371287859d373e7dcb4b42
Yosuke Mori <jolcde75+github@gmail.com>
2024-11-19 00:30:44 +0900

    組み込み関数minを使ってみる

--- algorithm/min_value/min_value.go
+++ algorithm/min_value/min_value.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
-func min(arrays []int, length int) int {
+func findMin(arrays []int, length int) int {
 	var minIndex = 0
 	for i := 1; i < length; i++ {
 		if arrays[minIndex] > arrays[i] {
@@ -14,6 +17,17 @@ func min(arrays []int, length int) int {
 
 func main() {
 	var score = []int{60, 80, 21, 92, 100, 4}
-	var minValue = min(score, len(score))
-	fmt.Printf("最小値は %d\n", minValue)
+	var minValue = findMin(score, len(score))
+	fmt.Printf("最小値は%d\n", minValue)
+
+	// 組み込み関数
+	// https://tip.golang.org/ref/spec#Min_and_max
+	fmt.Println(slices.Min(score)) // sliceの場合
+	fmt.Println(min(40, 23, 12, 43))
+	fmt.Println(min("c", "a", "b")) // アルファベット順
 }
+
+//=> 最小値は 4
+//4
+//12
+//a
