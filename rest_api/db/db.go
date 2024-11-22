db1d26e38a94e46dd26ddb87af26c611ef0766c6
Yosuke Mori <jolcde75+github@gmail.com>
2024-11-23 01:05:08 +0900

    .envをロードできるように修正

--- rest_api/db/db.go
+++ rest_api/db/db.go
@@ -10,28 +10,35 @@ import (
 )
 
 func NewDB() *gorm.DB {
-	if os.Getenv("ENV") == "development" {
-		err := godotenv.Load("../.env")
+	if os.Getenv("GO_ENV") == "dev" {
+		err := godotenv.Load()
 		if err != nil {
-			log.Fatal("Error loading .env file:")
+			log.Fatalf("Error loading .env file: %v", err)
 		}
 	}
-	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
+
+	url := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DB"))
+		os.Getenv("POSTGRES_DB"),
+	)
 	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Error connecting to database:")
+		log.Fatalf("Error connecting to database. URL: %s, Error: %v", url, err)
 	}
 	fmt.Println("Connected to database")
 	return db
 }
 
 func CloseDB(db *gorm.DB) {
-	sqlDB, _ := db.DB()
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Error getting database instance: %v", err)
+	}
 	if err := sqlDB.Close(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error closing database connection: %v", err)
 	}
 }
